9684d64a4f4c08f9c1e8f9ebd7fd31d432ca096a
Jakobi0 <gujakobi10@gmail.com>
2019-08-19 19:34:22 -0300

    Update meulogin.js

--- meulogin.js
+++ meulogin.js
@@ -5,7 +5,10 @@ let senha = prompt("Confirme a sua senha")
 if (senha.toLocaleLowerCase=="jakobifoda"){
     window.location="matematica.html"; done = 1 
 }
-else (done==0){
-    alert="erro";
+else {
+    done = 0
+}
+
+if(done==0){
     window.location=""
 }
